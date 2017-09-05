51eb32807959834c5703230840a4b6548114da00
Alejandro Gil <ajgp8821@gmail.com>
2017-09-05 03:27:16 -0430

    Empleado

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado';
 
 @Component({
   selector: 'app-empleado',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
 })
 
 export class EmpleadoComponent {
-   public titulo = 'Titulo del componente empleado';
+
+  public titulo = 'Componente Empleados';
+
+  public empleado: Empleado;
+  public trabajadores: Array<Empleado>;
+
+  constructor() {
+    this.empleado = new Empleado('Alejandro Gil', 28, 'Líder', true);
+    this.trabajadores = [
+      new Empleado('Manolo Martínes', 32, 'Gerente', true),
+      new Empleado('Ana López', 30, 'Analista', true),
+      new Empleado('Carlos Pérez', 25, 'Desarrollador', false)
+    ]
+  }
+
+  ngOnInit() {
+    // console.log(this.empleado);
+  }
 }
--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -12,10 +12,43 @@ export class FrutaComponent {
   public nombre_componente = 'Componente de fruta';
   public listado_frutas = 'Naranja, Manzana, Pera y Sandias';
 
-  public nombre = 'Alejandro';
-  public edad = 28;
-  public mayorDeEdad = true;
+  public nombre: string;
+  public edad: number;
+  public mayorDeEdad: boolean;
   public trabajos: Array<any> = ['Carpintero', 44, 'Fontanero'];
-  comodin: any = 23;
+  comodin: any;
 
+  constructor() {
+    this.nombre = 'Alejandro Gil';
+    this.edad = 28;
+    this.mayorDeEdad = true;
+    this.comodin = 'SI';
+    // console.log(this.trabajos);
+  }
+
+  ngOnInit() {
+    this.cambiarNombre();
+    this.cambiarEdad(3);
+    // alert(this.nombre + ' ' + this.edad);
+
+    // Variables y alcance
+    var uno = 8;
+    var dos = 15;
+    
+    if(uno == 8) {
+      let uno = 3;
+      var dos = 88;
+      
+      console.log('DENTRO DEL IF: ' + uno);
+    }
+    console.log('DENTRO DEL IF: ' + uno);
+  }
+
+  cambiarNombre() {
+    this.nombre = 'Violetha Gil';
+  }
+
+  cambiarEdad(edad) {
+    this.edad = edad;
+  }
 }
