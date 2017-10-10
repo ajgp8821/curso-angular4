bfeaa7bee428dc6ee9f51a6438399e1f0c1ecd23
Alejandro Gil <agil@consisint.com>
2017-10-10 17:41:40 -0400

    Rutas y parámetros por la url, redirecciones con el router

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -14,6 +14,7 @@ const appRoutes: Routes = [
     {path: 'fruta', component: FrutaComponent},
     {path: 'home', component: HomeComponent},
     {path: 'contacto', component: ContactoComponent},
+    {path: 'contacto/:page', component: ContactoComponent},
     {path: '**', component: HomeComponent} // Si la ruta no existe
 ];
 
--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
     selector: 'contacto',
@@ -7,5 +8,30 @@ import { Component } from '@angular/core';
 
 export class ContactoComponent{
     public titulo = "Página de contacto de la web";
+    public parametro;
+
+    constructor(
+        private _route: ActivatedRoute, // Instancia el objeto y el servicio
+        private _router: Router
+    ){}
+
+    ngOnInit(){
+        // Recogemos el parametro de la Url
+        this._route.params.forEach(
+            // Se usa funcion de flecha para poder acceder a this.parametro
+            (params: Params) => {
+            this.parametro = params['page'];
+            console.log(params);
+        });
+    }
+
+    redirigir(){
+        // ['/ruta', 'parametro']
+        this._router.navigate(['/contacto', 'homelab.com']);
+    }
+
+    redirigirDos(){
+        this._router.navigate(['/home']);
+    }
 }
 
