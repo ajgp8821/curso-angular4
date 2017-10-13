0796fb42fc645c7e5b72a36790fd8af9fd86d620
Alejandro Gil <agil@consisint.com>
2017-10-13 17:47:20 -0400

    Ejercicio con los servicios: agregar y eliminar ropa con un array

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,28 @@ import { RopaService } from '../services/ropa.service';
 
 export class HomeComponent{
     public titulo = "Página principal";
+    public listadoRopa: Array<string>;
+    public prendaAGuardar:string;
 
     constructor( // _servicio
         private _ropaService : RopaService // Se instancia, sin necesidad de new RopaService
     ){}
 
     ngOnInit(){
+        // Seteo a listadoRopa el valor de getRopa de ropa.service.ts
+        this.listadoRopa = this._ropaService.getRopa();
         console.log(this._ropaService.prueba('Camiseta Nike'));
+        console.log(this.listadoRopa);
+    }
+
+    guardarPrenda(){
+        this._ropaService.addRopa(this.prendaAGuardar);
+        this.prendaAGuardar = null; // deja vacio el campo luego de agregar
+    }
+
+    eliminarPrenda(index:number){
+        this._ropaService.deleteRopa(index);
+        // alert(index);
     }
 }
 
--- src/app/services/ropa.service.ts
+++ src/app/services/ropa.service.ts
@@ -6,8 +6,23 @@ import { Injectable } from '@angular/core';
 export class RopaService{
 
     public nombrePrenda: string = 'Pantalones Vaqueros';
+    public coleccionRopa: Array<string> = ['Pantalones Blancos', 'Camiseta Roja']
 
     prueba(nombrePrenda){
         return nombrePrenda;
+    }
+
+    addRopa(nombrePrenda):Array<string>{
+        this.coleccionRopa.push(nombrePrenda);
+        return this.getRopa();
+    }
+
+    getRopa():Array<string>{
+        return this.coleccionRopa;
+    }
+
+    deleteRopa(index:number){
+        this.coleccionRopa.splice(index, 1);
+        return this.getRopa();
     }
 }
